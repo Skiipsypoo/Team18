f777c8b1bf67afdc6c4344d6b793e1e5d453be44
Fredrik Meltveit <emkf.meltveitf@gmail.com>
2019-02-07 12:42:02 +0100

    Add tasks

--- fun-go/temp/bubble_sort_test.go
+++ fun-go/temp/bubble_sort_test.go
@@ -8,7 +8,6 @@ func TestBubbleSort(t *testing.T) {
 	for i := 0; i < len(arr) - 1; i++ {
 		if arr[i] < arr[i + 1] {
 			t.Errorf("Incorrect sorting")
-
 		}
 	}
 }
---
+++ fun-go/temp/sum.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func sum(i, j int) int {
+	return i + j
+}
+
+func main() {
+	fmt.Println(sum(5, 5))
+}
+
