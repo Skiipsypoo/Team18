57c5db6fbf527280c49ba02c637497a2e5af07f8
Fredrik Meltveit <emkf.meltveitf@gmail.com>
2019-02-07 12:25:10 +0100

    Add testing for bubble sort

--- fun-go/bubble_sort_test.go
+++ fun-go/bubble_sort_test.go
@@ -3,13 +3,12 @@ package main
 import "testing"
 
 func TestBubbleSort(t *testing.T) {
-	arr := {9, 8, 7, 6, 5, 4}
+	arr := []int {9, 8, 7, 6, 5, 4}
 
 	for i := 0; i < len(arr) - 1; i++ {
-		if arr[i] > arr[i + 1] {
+		if arr[i] < arr[i + 1] {
 			t.Errorf("Incorrect sorting")
-		} else {
-			t.Errorf("Not incorrect, just testing")
+
 		}
 	}
 }
