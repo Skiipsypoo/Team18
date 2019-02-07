d97fffc183587e6cbb06d44f430466d82e629b35
Fredrik Meltveit <emkf.meltveitf@gmail.com>
2019-02-07 11:27:18 +0100

    Add bubble sort

---
+++ bubble_sort.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) []int {
+	var swapped bool
+	for i := 0; i < len(arr) - 1; i++ {
+		swapped = false
+		for j := 0; j < len(arr) - i - 1; j++ {
+			if (arr[j] > arr[j + 1]) {
+				temp := arr[j]
+				arr[j] = arr[j + 1]
+				arr[j + 1] = temp 
+				swapped = true
+			}
+		}
+		if swapped == false {
+			break 
+		}
+	}
+	return arr 
+}
+
+func main() {
+	arr := []int{7, 4, 3, 6, 5, 1, 2, 9, 8}
+	arr = bubbleSort(arr)
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+
+}
